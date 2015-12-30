2df9a84ef07793a7b30b331688cc1a15ff56e1ea
Vinoth Energetic <vinothcse2000@gmail.com>
2015-12-30 19:00:27 +0530

    Create dereferencing_function_variable.cpp

---
+++ PHN-CodeUpload/dereferencing_function_variable.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+int myFunction()
+{
+  printf("\n myFunction");
+
+   return 0;
+}
+
+int myFunction2()
+{
+   printf("\n myFunction2");
+   return 0;
+}
+
+int myFunction3()
+{
+   printf("\n myFunction3");
+
+   return 0;
+}
