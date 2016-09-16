717931259a61645e7126e66175a17e410418373e
Vinoth Energetic <vinothcse2000@gmail.com>
2016-09-16 19:52:03 +0530

    Create array_subscript_operator_behaviour.cpp

---
+++ PHN-CodeUpload/array_subscript_operator_behaviour.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+
+
+
+int main()
+{
+
+
+   int a[10]={1,2,3};
+   printf("\n  :: %d ", a[1] );
+   //printf("\n  :: %d ", [1]a );
+   printf("\n  :: %d ", 1[a] );
+   //printf("\n  :: %d ", [1]a );
+
+   return 0;
+}
+
